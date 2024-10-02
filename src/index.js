62df484f258920bdcc8ef970afff3d65f6d92d93
Luis Thiago Batista da Sena <Luis.Sena@lbv.org.br>
2024-10-02 11:19:38 -0300

    teste de api 3

--- src/index.js
+++ src/index.js
@@ -47,6 +47,7 @@ app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "*"); // Permitindo todos os métodos HTTP
     res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
     next();
+    express.json()
 });
 
 /*
